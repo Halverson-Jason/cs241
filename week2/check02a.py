e372dfdf9283b420028d0f67c3c04d71e81112b2
Jason Halverson <halv.jason@gmail.com>
2019-04-22 20:43:44 -0700

    working pre-test bed

---
+++ week2/check02a.py
@@ -0,0 +1,21 @@
+def prompt_number():
+    while True:
+        number = int(input("Enter a positive number: "))
+        if number > 0:
+            return number
+        else:
+            print("Invalid entry. The number must be positive.")
+
+def compute_sum(num1, num2, num3):
+    return num1 + num2 + num3
+
+def main():
+    num1 = prompt_number()
+    num2 = prompt_number()
+    num3 = prompt_number() 
+    sum = compute_sum(num1,num2,num3)
+    
+    print("\nThe sum is: %d" % (sum))
+
+if __name__ == "__main__":
+    main()
