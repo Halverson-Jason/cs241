fea6034633372f5774b2200b644c5c8c90583647
Jason Halverson <halv.jason@gmail.com>
2019-04-22 20:58:36 -0700

    passing testbed

--- week2/check02a.py
+++ week2/check02a.py
@@ -2,6 +2,8 @@ def prompt_number():
     while True:
         number = int(input("Enter a positive number: "))
         if number > 0:
+            #This print is for the stupid newline issue in test-bed
+            print()
             return number
         else:
             print("Invalid entry. The number must be positive.")
@@ -12,10 +14,10 @@ def compute_sum(num1, num2, num3):
 def main():
     num1 = prompt_number()
     num2 = prompt_number()
-    num3 = prompt_number() 
+    
     sum = compute_sum(num1,num2,num3)
     
-    print("\nThe sum is: %d" % (sum))
+    print("The sum is: %d" % (sum))
 
 if __name__ == "__main__":
     main()
