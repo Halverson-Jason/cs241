6889f0ff515498d819d771e67819205c0bec1dbc
Jason Halverson <halv.jason@gmail.com>
2019-04-26 14:43:09 -0700

    added skeleton for assign02

---
+++ week2/assign02.py
@@ -0,0 +1,27 @@
+
+# Sample Output
+        # Please enter the data file: /home/cs241/assign02/rates.csv
+
+        # The average commercial rate is: 0.08402623352821378
+
+        # The highest rate is:
+        # Napakiak Ircinraq Power Co (99634, AK) - $0.839779005525
+
+        # The lowest rate is:
+        # Sierra Pacific Power Co (89496, NV) - $0.0
+
+# Prompt the user for a filename.
+
+# Open the requested file and read through it line by line.
+
+# Ignore the first line, as it contains header information.
+
+# Find the column for comm_rate and keep track of it as needed. (You may assume the file will have a consistent ordering of columns.)
+
+# After parsing through the complete file, display the average (mean) commercial rate across all zip codes.
+
+# Display the utility company, zip code, state, and rate for the zip code with the highest commercial rate in the file.
+
+# Display the utility company, zip code, state, and rate for the zip code with the lowest commercial rate in the file.
+
+# If there is a tie for the highest or lowest rate, you should display the zip code that came first in the file.
