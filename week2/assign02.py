137e893e771f700e53d780dd42e10d09da30792d
Jason Halverson <halv.jason@gmail.com>
2019-05-01 15:50:20 -0700

    assign02 added parsing of data / abstraction

--- week2/assign02.py
+++ week2/assign02.py
@@ -8,12 +8,9 @@ def open_file(file_name):
 
 # Read through it line by line.
 def get_lines(user_file):
-    rate_lowest = 1.0
-    rate_highest = 0.0
-    rate_sum = 0
-    rate_count = 0
-    rate_highest_line = []
-    rate_lowest_line = []
+    rate_highest, rate_lowest = 0.0, 1.0
+    rate_sum,rate_count = 0,0
+    rate_highest_line,rate_lowest_line = "",""
 
     # Ignore the first line, as it contains header information.
     for line in user_file[1:]:
@@ -29,21 +26,33 @@ def get_lines(user_file):
         rate_count +=1
     rate_average = rate_sum / rate_count
     print("\nThe average commercial rate is: {}\n".format(rate_average))
+    
+    #TODO: remove these and place then in the appropriate place
+    print_highest_rate(parse_rate(rate_highest_line))
+    print_lowest_rate(parse_rate(rate_lowest_line))
 
-    lowest_rate = rate_lowest_line
-    company_name_lowest = lowest_rate[2]
-    zip_lowest = lowest_rate[0]
-    state_lowest = lowest_rate[3]
-    final_rate_lowest = float(lowest_rate[6])
+#parses the line into a rate dictionary
+def parse_rate(data):
+    rate = {'Company': data[2], 'Zip': data[0], 'State': data[3], 'Rate': float(data[6]) }
+    return rate
 
-    highest_rate = rate_highest_line
-    company_name_highest = highest_rate[2]
-    zip_highest = highest_rate[0]
-    state_highest = highest_rate[3]
-    final_rate_highest = highest_rate[6]
+#prints the lowest rate
+def print_lowest_rate(rate):
+    #TODO: refactor the parsing of data
+    # company_name_lowest = rate[2]
+    # zip_lowest = rate[0]
+    # state_lowest = rate[3]
+    # final_rate_lowest = float(rate[6])
+    print("The lowest rate is:\n",rate['Company']," (",rate['Zip'],", ",rate['State'],") - $",rate['Rate'])
 
-    print("The highest rate is:\n{} ({}, {}) - ${}\n".format(company_name_highest,zip_highest,state_highest,final_rate_highest))
-    print("The lowest rate is:\n{} ({}, {}) - ${}".format(company_name_lowest,zip_lowest,state_lowest,final_rate_lowest))
+#prints the highest rate
+def print_highest_rate(rate):
+    # company_name_highest = rate[2]
+    # zip_highest = rate[0]
+    # state_highest = rate[3]
+    # final_rate_highest = rate[6]
+    print("The highest rate is:\n",rate['Company']," (",rate['Zip'],", ",rate['State'],") - $",rate['Rate'])
+    
 
 
 def main():
