21abf9c0255553704b6c50623990fedd5689c9b7
Jason Halverson <halv.jason@gmail.com>
2019-05-09 12:59:26 -0700

    added structure to game play

--- week3/prove03.py
+++ week3/prove03.py
@@ -20,20 +20,24 @@ class Robot:
     """
     INITIAL_FUEL = 100
     INITIAL_COORD = 10
-    DECREASE_FUEL = 15
+    DECREASE_FUEL_FIRE = 15
+	DECREASE_FUEL_MOVE = 5
+	
     def __init__(self):
         set_fuel_level(INITIAL_FUEL)
         _location = {"x-coordinate":INITIAL_COORD, "y-coordinate":INITIAL_COORD}
 
     def fire():
         print("Pew! Pew!")
-        set_fuel_level()
+        decrease_fuel_level(DECREASE_FUEL_FIRE)
 
     def display(self):
         print("({}, {}) - Fuel: {}".format(self._location["x-coordinate"],self._location["y-coordinate"], self._fuel))
-#TODO: FINISH THE MOVE METHOD
-    def move(direction):
-        set_x-coordinate(direction)
+
+    def move(x-coord,y-coord):
+        set_x-coordinate(x-coord)
+		set_y-coordinate(y-coord)
+		decrease_fuel_level(DECREASE_FUEL_MOVE)
 
     def set_x-coordinate(self,coordinate):
         self._location["x-coordinate"] = coordinate
@@ -41,10 +45,49 @@ class Robot:
     def set_y-coordinate(self,coordinate):
         self._location["y-coordinate"] = coordinate
 
-    def set_fuel_level(self, level=None):
-        """This is overloaded to either set the fuel level or decrease it if called"""
-        if level is not None:
-            self._fuel = level
-        else:
-            self._fuel = self._fuel - DECREASE_FUEL
+    def set_fuel_level(self, level):
+        self._fuel = level
+		
+	def decrease_fuel_level(self, decrease_amount):
+		self._fuel = self._fuel + decrease_amount
+	def insufficient_fuel():
+		print()
+
+def prompt_command():
+	return input("Enter command: ")
+	
 #TODO: Create main class and game logic
+def play_game()
+	"""returns true if use wants to play again"""
+	robot = Robot()
+	user_command = prompt_command()
+	while(input_correct):
+		switch(user_command):
+			case "LEFT":
+				robot.move(-1,0)
+			case "RIGHT":
+				robot.move(1,0)
+			case "UP":
+				robot.move(0,-1)
+			case "DOWN":
+				robot.move(0,1)
+			case "FIRE":
+				robot.fire()
+			case "STATUS":
+				robot.display()
+			case "QUIT":
+				return false
+			default:
+				print("")
+				input_correct = False
+		
+	
+	
+	
+def main():
+	play = True
+	while(play):
+		play = play_game()	
+	print("Goodbye")
+if __init__ == "__main__":
+	main()
