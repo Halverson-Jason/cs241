f6089eb0259785e33f76c64adfe062001df0aaca
Jason Halverson <halv.jason@gmail.com>
2019-06-12 22:15:32 -0700

    Asteroids - Flying object wrap working

--- week8/asteroids/flyingObject.py
+++ week8/asteroids/flyingObject.py
@@ -1,12 +1,18 @@
 from point import Point
 from velocity import Velocity
 
+# These are Global constants to use throughout the game
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
 class FlyingObject:
     """The FlyingObject has all the"""
     def __init__(self,starting_point = Point(200,200)):
         self.alive = True
         self.velocity = Velocity(0,0)
         self.center = starting_point
+        self.isWrapX = False
+        self.isWrapY = False
 
     def isAlive(self):
         return self.alive
@@ -18,10 +24,35 @@ class FlyingObject:
         if velocity != None:
             self.velocity = velocity
         elif dx != None and dy != None:
+            #TODO: Fix to fit properties
             self.velocity.set_dx(dx)
             self.velocity.set_dy(dy)
 
     def advance(self):
+
+        if not self.isWrapX and self.center.center_x >= SCREEN_WIDTH:
+            #TODO: Fix accessing private variables...
+            self.center.x = 0
+            self.isWrapX = True
+            print(self.center.center_x)
+
+        elif not self.isWrapX and self.center.center_x <= 0:
+            self.center.center_x = SCREEN_WIDTH
+            self.isWrapX = True
+
+        elif not self.isWrapY and self.center.center_y >= SCREEN_HEIGHT:
+            #TODO: Fix accessing private variables...
+            self.center.y = 0
+            self.isWrapY = True
+
+        elif not self.isWrapY and self.center.center_y <= 0 :
+            self.center.center_y = SCREEN_HEIGHT
+            self.isWrapY = True
+
+        else:
+            self.isWrapX = False
+            self.isWrapY = False
+
         self.center.center_x = self.velocity.dx
         self.center.center_y = self.velocity.dy
 
